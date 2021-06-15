0afa0194e9c3a31dcb578d2efca59b3a79981307
jean PORCHER <jean.porcher@socgen.com>
2021-06-15 18:43:16 +0200

    change  port

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "/index.html"));
 });
 
-app.listen(port, function () {
-  console.log("CORS-enabled web server listening on port " + port);
+app.listen(3000, function () {
+  console.log("CORS-enabled web server listening on port " + 3000);
 });
