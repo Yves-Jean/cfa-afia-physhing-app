0dead158df3c9d423b2ba526c76d71b7cccb0bfb
jean PORCHER <jean.porcher@socgen.com>
2021-06-15 18:39:22 +0200

    adding favicon and node server

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const path = require("path");
+const favicon = require("serve-favicon");
+dotenv.config();
+const cors = require("cors");
+const app = express();
+const port = process.env.port;
+
+app.use(cors());
+
+app.use(express.static("public"));
+
+app.use(favicon(path.join(__dirname, "public", "AFIA_files", "favicon.ico")));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+app.listen(port, function () {
+  console.log("CORS-enabled web server listening on port " + port);
+});
